a199da21e831731c7e6f3ea4b507325d4477e5e3
sanikagurav <143166530+sanikagurav@users.noreply.github.com>
2023-08-29 21:20:09 +0530

    reverse.cpp

---
+++ reverse.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void reverse(int A[], int size)
+{
+    for(int i=size-1; i>=0; i--)
+    {
+        cout << A[i] << " ";
+    }
+}
+int main() 
+{
+     
+    int size;
+    cin >> size;
+    int A[size];
+    
+    for(int i=0; i<size; i++)
+    {
+        cin >> A[i];
+    } 
+    reverse(A, size);
+    return 0;
+}
