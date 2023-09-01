7c1bb51d920f5049af0b690329ce9084e9bb85c3
sanikagurav <143166530+sanikagurav@users.noreply.github.com>
2023-09-01 22:38:44 +0530

    SwapNumber.cpp
    Program to Swap Two Numbers without using third variable

---
+++ SwapNumber.cpp
@@ -0,0 +1,27 @@
+/*  C++ Program to Swap Two Numbers without using third variable  */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a,b ;
+
+    cout<<"Enter 1st number :: ";
+    cin>>a;
+    cout<<"\nEnter 2nd number :: ";
+    cin>>b;
+
+    cout << "\nBefore swapping, Numbers are :: " << endl;
+    cout << "\n\ta = " << a << ", b = " << b << endl;
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    cout << "\nAfter swapping, Numbers are :: " << endl;
+    cout << "\n\ta = " << a << ", b = " << b << endl;
+
+    return 0;
+}
